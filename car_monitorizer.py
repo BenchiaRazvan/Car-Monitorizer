4b09d4f10ec0a892d972a7ce43aec2babc57cd1d
BenchiaRazvan <79082384+BenchiaRazvan@users.noreply.github.com>
2021-11-29 17:17:42 +0200

    Get data from best offer section

--- car_monitorizer.py
+++ car_monitorizer.py
@@ -2,12 +2,45 @@ import requests
 from bs4 import BeautifulSoup 
 
 
+url = "https://www.autovit.ro/"
+f = requests.get(url)
+soup = BeautifulSoup(f.content, 'lxml')
+
+
+def best_offer():
+
+    cars = soup.find('section', {
+        'class' : 'e1a4cfxe1'
+    }).find_all('a')
+
+    informations_list = []
+
+    car_title = soup.find('h3', { 'class' : 'e1lmj3dz0'}).text
+    car_price = soup.find('div', { 'class': 'evznqyf1'}).text
+
+    car_informations = soup.find('ul', { 'class': 'optimus-app-13n8fh9 e16henwp0'}).find_all('span')
+    for car in car_informations:
+        informations_list.append(car.text)
+
+    car_year = informations_list[0]
+
+    for inf in informations_list:
+        if(inf.endswith('km')):
+            car_km = inf
+
+    json_output = {
+        'name': car_title,
+        'price': car_price,
+        'year': car_year,
+        'km' : car_km,
+    }
+    print(json_output)      
+
+def daily_offer():
+    print("test")
 
 def main():
-    url = "https://www.autovit.ro/"
-    f = requests.get(url)
-    soup = BeautifulSoup(f.content, 'lxml')
-    print(soup)
+    best_offer()
 
 if __name__=="__main__":
     main()
